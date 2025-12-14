eb67cf78072017242521999180aa01c03e768d59
Alex Candido <alex.candido.tec@gmail.com>
2025-12-14 12:31:07 -0300

    implement model associations and validations

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,22 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: carts
+#
+#  id           :bigint           not null, primary key
+#  total_price  :decimal(10, 2)   default(0.0), not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  status       :integer          default("active"), not null
+#  abandoned_at :datetime
+#
 class Cart < ApplicationRecord
+  has_many :cart_items, dependent: :destroy
+  has_many :products, through: :cart_items
+
   validates_numericality_of :total_price, greater_than_or_equal_to: 0
 
+  enum status: { active: 0, inactive: 1, abandoned: 2 }
   # TODO: lógica para marcar o carrinho como abandonado e remover se abandonado
 end
