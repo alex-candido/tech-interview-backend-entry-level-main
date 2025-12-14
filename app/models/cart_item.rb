d6e5f0dca0fea24dbbe36e1332c5592f219e8740
Alex Candido <alex.candido.tec@gmail.com>
2025-12-14 12:31:07 -0300

    implement model associations and validations

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,19 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: cart_items
+#
+#  id         :bigint           not null, primary key
+#  cart_id    :bigint           not null
+#  product_id :bigint           not null
+#  quantity   :integer          default(1), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class CartItem < ApplicationRecord
   belongs_to :cart
   belongs_to :product
+
+  validates :quantity, numericality: { greater_than: 0 }
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,19 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: products
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  unit_price :decimal(10, 2)   not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Product < ApplicationRecord
-  validates_presence_of :name, :price
-  validates_numericality_of :price, greater_than_or_equal_to: 0
+  has_many :cart_items, dependent: :destroy
+  has_many :carts, through: :cart_items
+
+  validates_presence_of :name, :unit_price
+  validates_numericality_of :unit_price, greater_than_or_equal_to: 0
 end
